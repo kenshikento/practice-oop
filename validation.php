0b349fe65233ee956185a097827d0d55452359ee
Tien <tien-nguyen1@hotmail.co.uk>
2020-03-04 18:38:12 +0000

    updating validation
    just rewriting the  checks

--- validation.php
+++ validation.php
@@ -2,44 +2,41 @@
 
 	class validation
 	{
+	    	/**
+			 * Checks if Currency name matches to the white list array
+	         *
+	         * @return String
+	         */			
+			public function currentvalidation(string $currency) 
+			{				
+				$whitelist = array('GBR','KRW','USD','VND','CNY');		
 			
-			public function currentvalidation($currency) 
-				{				
-					
-					$whitelist = array('GBR','KRW','USD','VND','CNY');		
-			
-						if(!in_array($currency,$whitelist))
-						{
-							throw new Exception("Please try another currency",1);
-						}
-						else 
-						{
-							return $currency;
-						}
-					
-						
+				if (!in_array($currency,$whitelist)) {
+					throw new Exception("Please try another currency",1);
 				}
-			public function pricevalidation($price)
+
+				return $currency;																
+			}
+
+	    	/**
+			 * Check if price is within the requirement 
+	         *
+	         * @return String
+	         */	
+			public function pricevalidation(float $price)
+			{
+				if ($price || $price === null || $price < 1) {
+					throw new Exception("Please try again",1);
+				}
+
+				if($price > 100)
 				{
-					if($price < 0)
-						{
-							throw new Exception("Please try again",1);
-						}				
-					else if($price > 100)
-						{
-							throw new Exception("Please try again max is 100",1);
-						}						
-						else 
-						{
-							return $price;
-						}
-				}
+					throw new Exception("Please try again max is 100",1);
+				}	
+
+				return $price;
+			}
 	
 	}
 
-
-
-
-
-?> 
 
